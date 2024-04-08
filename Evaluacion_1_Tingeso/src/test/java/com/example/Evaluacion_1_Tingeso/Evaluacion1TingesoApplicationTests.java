66f7e30e9d9be566eb174672e87c284693c48cd7
Fernando Solis Paz <fernando.solis.p@usach.cl>
2024-04-08 17:15:57 -0400

    initial commit

---
+++ Evaluacion_1_Tingeso/src/test/java/com/example/Evaluacion_1_Tingeso/Evaluacion1TingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Evaluacion_1_Tingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion1TingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
