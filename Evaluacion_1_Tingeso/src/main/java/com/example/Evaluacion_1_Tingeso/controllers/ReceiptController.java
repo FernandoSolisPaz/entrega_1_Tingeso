f178717748bd3d9690101b04c3c5a0ecff50a2b4
Fernando Solis Paz <fernando.solis.p@usach.cl>
2024-04-11 01:15:14 -0400

    minor bug fixed

--- Evaluacion_1_Tingeso/src/main/java/com/example/Evaluacion_1_Tingeso/controllers/ReceiptController.java
+++ Evaluacion_1_Tingeso/src/main/java/com/example/Evaluacion_1_Tingeso/controllers/ReceiptController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/repairs")
+@RequestMapping("/api/receipts")
 @CrossOrigin("*")
 public class ReceiptController {
     @Autowired
