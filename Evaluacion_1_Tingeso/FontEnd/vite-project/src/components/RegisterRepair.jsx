2b9f9527ebe77633fae1ee97142cac496c2e256e
Fernando Solis Paz <fernando.solis.p@usach.cl>
2024-04-26 09:42:41 -0400

    receipt component modified

--- Evaluacion_1_Tingeso/FontEnd/vite-project/src/components/RegisterRepair.jsx
+++ Evaluacion_1_Tingeso/FontEnd/vite-project/src/components/RegisterRepair.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
+import carService from "../services/car.service.js";
 import receiptService from "../services/receipt.service.js";
+import repairService from "../services/repair.service.js";
 import { useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -13,15 +15,32 @@ const RegisterRepair = () => {
     const [carPlate, setCarPlate] = useState("");
     const [camp2Habilitated, setCamp2] = useState(false);
     const [repairIds, setRepairIds] = useState([]);
+    const [realRepairIds, setRealRepairIds] = useState([]);
+    const [repairEntityv, setRepairEntity] = useState(null);
+    const [carEntity, setCarEntity] = useState(null);
+    const [carMotorId, setCarMotorId] = useState(-1);
     const navigate = useNavigate();
     let tittle = "Register a repair for a car";
 
-    const saveReceipt = (r) =>{
+    const saveReceipt = async (r) =>{
         r.preventDefault();
+        setCarEntity(carService.get(carPlate));
+        setCarMotorId(carEntity.motor);
+        for (let i = 0; i < repairIds.length; i++) {
+            const Name = repairIds[i];
+            try {
+                const repairEntity = await repairService.getByMotorAndName(carMotorId,Name);
+                setRepairEntity(repairEntity);
+                // eslint-disable-next-line
+                setRealRepairIds([...realRepairIds, repairEntityv.repairId])
+            } catch (error) {
+                console.error('Error trying to bring the Repair:', error);
+            }
 
+        }
         const receipt = {carPlate}
         receiptService
-            .create(receipt)
+            .create(receipt, realRepairIds)
             .then(response => {
                 console.log("Receipt added successfully", response.data);
                 navigate("/home");
@@ -33,6 +52,8 @@ const RegisterRepair = () => {
             });
 
     }
+
+
 
     const handleCarPlateChange = (event) => {
         const valor = event.target.value;
@@ -82,18 +103,58 @@ const RegisterRepair = () => {
                     <legend>Options of Repairs:</legend>
                     <FormControlLabel
                         control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
-                        label="Opción 1"
-                        value={0}
+                        label="Braking System"
+                        value="Braking System"
                     />
                     <FormControlLabel
                         control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
-                        label="Opción 2"
-                        value={1}
+                        label="Refrigeration System"
+                        value="Refrigeration System"
                     />
                     <FormControlLabel
                         control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
-                        label="Opción 3"
-                        value={2}
+                        label="Motor"
+                        value="Motor"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="Transmision"
+                        value="Transmision"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="Electric System"
+                        value="Electric System"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="Escape System"
+                        value="Escape System"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="Tyres and Wheels"
+                        value="Tyres and Wheels"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="Suspension and steering"
+                        value="Suspension and steering"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="AC and Heating System"
+                        value="AC and Heating System"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="Fuel System"
+                        value="Fuel System"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleCheckboxChange} disabled={!camp2Habilitated}/>}
+                        label="Windshield and Glass"
+                        value="Windshield and Glass"
                     />
                 </FormControl>
                 <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
