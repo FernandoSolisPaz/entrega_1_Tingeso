31ea515d8e615e6e89136baf5260d01546cbcccd
FernandoSolísPaz <fernando.solis.p@usach.cl>
2024-05-02 01:22:20 -0400

    simulacro 2

--- Evaluacion_1_Tingeso/FontEnd/vite-project/src/components/Home.jsx
+++ Evaluacion_1_Tingeso/FontEnd/vite-project/src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
                 AutoFix es una aplicación web para gestionar planillas de reparaciones de
                 autos. Esta aplicación ha sido desarrollada usando tecnologías como{" "}
                 <a href="https://spring.io/projects/spring-boot">Spring Boot</a> (para
-                el Backend), <a href="https://reactjs.org/">React</a> (para el Frontend)
+                el backend), <a href="https://reactjs.org/">React</a> (para el Frontend)
                 y <a href="https://www.mysql.com/products/community/">MySQL</a> (para la
                 base de datos).
             </p>
